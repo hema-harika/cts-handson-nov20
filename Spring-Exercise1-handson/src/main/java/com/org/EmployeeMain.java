fc67668462835836cb2695d497535be54a0b9815
harika <hemaharikad@gmail.com>
2020-12-01 21:33:39 +0530

    handson added

---
+++ Spring-Exercise1-handson/src/main/java/com/org/EmployeeMain.java
@@ -0,0 +1,10 @@
+package com.org;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+
+		EmployeeService service = new EmployeeService();
+		service.storeEmployee();
+	}
+}
---
+++ Spring-Exercise1-handson/src/main/java/com/org/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.org;
+
+public class EmployeeService {
+	EmployeeDao employeeDao = new EmployeeDao();
+	public void storeEmployee() {
+		System.out.println("storeEmployee() method of service class");
+		employeeDao.store();
+	}
+}
